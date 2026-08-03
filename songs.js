4ffc208de31c92bf8dde29f16d111696e9e075bb
madenjanna-ui <madenjanna@gmail.com>
2026-08-03 09:04:55 +0400

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,40 @@
+const songs = [
+  {
+    id: 1,
+    title: "Комета",
+    artist: "Денис",
+    duration: "3:42",
+
+    audio: "audio/kometa.mp3",
+    cover: "covers/kometa.jpg",
+    lyrics: "lyrics/kometa.txt",
+
+    category: "Любовь"
+  },
+
+  {
+    id: 2,
+    title: "Ты лилия",
+    artist: "Денис",
+    duration: "4:08",
+
+    audio: "audio/ty-liliya.mp3",
+    cover: "covers/ty-liliya.jpg",
+    lyrics: "lyrics/ty-liliya.txt",
+
+    category: "Любовь"
+  },
+
+  {
+    id: 3,
+    title: "История любви",
+    artist: "Денис",
+    duration: "4:51",
+
+    audio: "audio/istoriya-lubvi.mp3",
+    cover: "covers/istoriya-lubvi.jpg",
+    lyrics: "lyrics/istoriya-lubvi.txt",
+
+    category: "Любовь"
+  }
+];
