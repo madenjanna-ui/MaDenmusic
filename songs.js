424b00192edce9956614f5c7d14e4204a337c1f7
madenjanna-ui <madenjanna@gmail.com>
2026-08-03 10:58:29 +0400

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,6 +1,54 @@
-const songs=[{
-id:1,title:'Демо песня',artist:'Денис',
-audio:'audio/demo.mp3',
-cover:'covers/demo.jpg',
-lyrics:'lyrics/demo.txt'
-}];
+const songs = [
+
+{
+    id:1,
+
+    title:"Ты лилия (Nordic Remix)",
+
+    artist:"MaDen",
+
+    audio:"Ты лилия (nordic remix).mp3",
+
+    cover:"Ты лилия.jpg",
+
+    lyrics:`
+Здесь будет текст песни
+"Ты лилия (Nordic Remix)"
+`
+},
+
+{
+    id:2,
+
+    title:"История Любви",
+
+    artist:"MaDen",
+
+    audio:"История Любви.mp3",
+
+    cover:"История Любви.jpg",
+
+    lyrics:`
+Здесь будет текст песни
+"История Любви"
+`
+},
+
+{
+    id:3,
+
+    title:"Сегодня мы считаем звёзды",
+
+    artist:"MaDen",
+
+    audio:"Сегодня мы считаем звёзды.mp3",
+
+    cover:"Сегодня мы считаем звёзды.jpg",
+
+    lyrics:`
+Здесь будет текст песни
+"Сегодня мы считаем звёзды"
+`
+}
+
+];
